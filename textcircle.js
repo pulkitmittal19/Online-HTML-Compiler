b25aa1cab40e04ff936b7798a2323cd03d4c0770
pulkitmittal19 <pulkitmittal19@gmail.com>
2020-04-14 23:19:27 +0530

    Add: Mongodb files

--- textcircle.js
+++ textcircle.js
@@ -1,5 +1,5 @@
 this.Documents = new Mongo.Collection("documents");
-
+EditingUsers = new Mongo.Collection('editingUsers')
 if (Meteor.isClient) {
 // find the first document in the Documents colleciton and send back its id
   Template.editor.helpers({
@@ -12,14 +12,15 @@ if (Meteor.isClient) {
         return undefined;
       }
     }, 
-    // template helper that configures the CodeMirror editor
-    // you might also want to experiment with the ACE editor
+   
     config:function(){
       return function(editor){
         editor.setOption("mode", "html");
         editor.on("change", function(cm_editor, info){
           //console.log(cm_editor.getValue());
           $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+          Meteor.call('addEditingUser')
+         // EditingUsers.insert({user:'pulkit'})
         });        
       }
     }, 
@@ -28,9 +29,39 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // startup code that creates a document in case there isn't one yet. 
+     
     if (!Documents.findOne()){// no documents yet!
         Documents.insert({title:"my new document"});
     }
   });
 }
+
+Meteor.methods({
+  addEditingUser: function(){
+    
+    var doc = Documents.findOne();
+    if (!doc){
+      return;
+    }
+
+    if (!this.userId){
+      return;
+    }
+
+    var user = Meteor.user().profile;
+
+     var eusers = EditingUsers.findOne({docid:doc._id})
+
+    if(!eusers){
+      eusers={
+        docid:doc._id,
+        users:{}
+      };
+    }
+      user.lastEdit= new Date();
+      eusers.users[this.userId] = user;
+
+
+      EditingUsers.upsert({id: eusers._id}, eusers)
+  }
+})
